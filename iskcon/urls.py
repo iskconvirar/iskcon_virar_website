922cfe08e0c59d3cb77814c8c2dee79bcdb85139
ISKCON Virar <dhruvnmahyavanshi2@gmail.com>
2025-08-01 20:46:15 +0530

    minor changes

--- iskcon/urls.py
+++ iskcon/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for isckon project.
+URL configuration for iskcon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from isckonapp.views import *
+from iskconapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- iskconapp/apps.py
+++ iskconapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IsckonappConfig(AppConfig):
+class iskconappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'isckonapp'
+    name = 'iskconapp'
