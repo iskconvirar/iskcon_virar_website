8d6ad09eeb4b307d60ca024aeac74c32c03fe7ae
Mayank-711 <148845605+Mayank-711@users.noreply.github.com>
2025-08-01 15:35:31 +0000

    added admin panel

--- isckon/urls.py
+++ isckon/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for iskcon project.
+URL configuration for isckon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from iskconapp.views import *
+from isckonapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
