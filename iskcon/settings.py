36f1d9294850aacb9635a2d7c8ca8036c8a6ab9c
ISKCON Virar <dhruvnmahyavanshi2@gmail.com>
2025-08-08 11:07:44 +0000

    neon postgre

--- iskcon/settings.py
+++ iskcon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,13 +76,14 @@ WSGI_APPLICATION = 'iskcon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ['DATABASE_URL'],
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
